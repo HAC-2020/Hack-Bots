60e5aa724e379928d213ff76a3d1a759fbdd08a0
Prajwal238 <prajwal_katakam@srmap.edu.in>
2020-08-07 19:49:40 +0530

    Deleted commits
    Commits deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hack-Bots
-This is the initiated README for team Hack Bots, Please edit this as you build your project
--- lib/config/palette.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Palette {
-  static const Color primaryColor = Colors.black;
-}
